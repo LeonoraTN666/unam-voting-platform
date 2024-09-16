e4f6c6eb9b86c78b79c39e900b19f6fc79c0c46c
LeonoraTN666 <110929383+LeonoraTN666@users.noreply.github.com>
2024-09-16 11:09:55 +0200

    Create confirmation.js

---
+++ confirmation.js
@@ -0,0 +1,9 @@
+// confirmation.js
+
+document.getElementById('backToCategoriesBtn').addEventListener('click', function() {
+    window.location.href = 'categories.html';
+});
+
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Redirect to login page
+});
